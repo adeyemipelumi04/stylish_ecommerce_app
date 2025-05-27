abe0000c57b4f8cd75ce880ccfa8ad77350c0521
Adeyemi Favour O <adeyemipelumi202@gmail.com>
2025-05-27 12:41:04 +0100

    Choose product screen done

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,8 +4,6 @@ class AppColors {
   static const Color primaryColor = Color(0xFFF83758);
   static const Color primaryColor2 = Color(0xFFFA7189);
   static const Color scaffoldBackground = Color(0xFFFDFDFD);
-  static const Color secondaryButton = Color(0xFF494848);
-  static const Color progressBarColor = Color(0xFF181818);
-  static const Color grey = Color(0xFF808080);
-  static const Color lightGreen = Color(0xFFDDFFCC);
+  static const Color blackColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color greyColor = Color(0xFFBDBDBD);
 }
