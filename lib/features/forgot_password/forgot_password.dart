e2671ab8a9b4f0207ce58d9d63313745447d2d2c
Adeyemi Favour O <adeyemipelumi202@gmail.com>
2025-05-28 10:51:51 +0100

    Forget password UI completed

---
+++ lib/features/forgot_password/forgot_password.dart
@@ -0,0 +1,100 @@
+import 'package:e_commerce_app/utils/colors.dart';
+import 'package:e_commerce_app/widgets/email_field.dart';
+import 'package:e_commerce_app/widgets/primary_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildTop(context),
+            SizedBox(height: 40.h),
+            _buildFormField(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTop(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: "Forgot\n",
+        style: TextStyle(
+          color: AppColors.blackColor,
+          fontSize: 36.h,
+          fontWeight: FontWeight.w600,
+          fontFamily: 'Montserrat',
+        ),
+        children: [
+          TextSpan(
+            text: "Password?",
+            style: TextStyle(
+              color: AppColors.blackColor,
+              fontSize: 36.h,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Montserrat',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget _buildFormField(BuildContext context) {
+  final emailController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  return Form(
+    key: formKey,
+    child: Column(
+      children: [
+        EmailInputField(
+          controller: emailController,
+          hint: "Enter your email address",
+          validator: (String? value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your email address';
+            }
+            // Simple email regex
+            final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+            // Username: at least 3 chars, no spaces
+
+            if (!emailRegex.hasMatch(value) &&
+                (value.length < 3 || value.contains(' '))) {
+              return 'Enter a valid email address';
+            }
+            return null;
+          },
+        ),
+        SizedBox(height: 20.h),
+        Text(
+          "We will send you an email with a link to reset your password.",
+          style: TextStyle(
+            fontSize: 14.sp,
+            color: AppColors.greyColor,
+            fontFamily: 'Montserrat',
+          ),
+        ),
+        SizedBox(height: 40.h),
+        PrimaryButton(
+          label: 'Submit',
+          onPressed: () {
+            if (formKey.currentState!.validate()) {
+              // Handle login logic
+            }
+          },
+          backgroundColor: AppColors.primaryColor,
+        ),
+      ],
+    ),
+  );
+}
